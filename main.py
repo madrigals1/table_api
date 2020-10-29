d92758123a31813f534a8bcb7469cd4f940e4bc7
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-10-29 20:14:57 +0700

    ADD static file hosting

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+STATIC_PATH = os.environ.get("STATIC_PATH")
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path=f"/{STATIC_PATH}")
 
 
 @app.route("/")
