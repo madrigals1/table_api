a6dea18649fce89aa0fab8b5eab8ddcd6b54ff56
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-10-31 13:46:31 +0700

    ADD debug mode

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from flask import Flask
-from src.constants import STATIC_PATH
+from src.constants import STATIC_PATH, FLASK_DEBUG
 from src.utils import create_png_from_dict
 from flask import request
 
-
+# Run app
+debug_mode = FLASK_DEBUG == 1
 app = Flask(__name__, static_url_path=f"/{STATIC_PATH}")
+app.run(debug=debug_mode)
 
 
 @app.route("/")
 def index():
-    return {"Detail": "Table API is working. Use /convert to convert dict into table PNG"}
+    return {
+        "Detail": "Table API is working. Use /convert to convert dict into table PNG"
+    }
 
 
 @app.route("/convert", methods=["POST"])
--- src/constants.py
+++ src/constants.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 STATIC_PATH = os.environ.get("STATIC_PATH")
+FLASK_DEBUG = os.environ.get("FLASK_DEBUG")
